aebd704690c28f5304a845bea65e212076910b99
Asif Azimi <asifazimi620@gmail.com>
2022-03-16 04:06:23 +0430

    App changed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <button className="border border-black p-3 m-3 hover:bg-gray-300">
         Click here
       </button>
+      <button className="font-bold border rounded px-3 py-2 border-red-500 transition-all ease-out hover:bg-pink-500 hover:text-white ">
+        Submit me
+      </button>
     </div>
   );
 }
