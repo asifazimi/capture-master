df5e69109e5941670a3d4a2c6ad5f2722266f1e9
Asif Azimi <asifazimi620@gmail.com>
2022-03-15 22:17:39 +0430

    initialise project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">React and Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
