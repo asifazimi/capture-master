d499c26c3f5a12683035c3862a315320f7545a7d
Asif Azimi <asifazimi620@gmail.com>
2022-03-16 03:36:31 +0430

    App changed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
       <h1 className="text-3xl font-bold underline text-pink-500">
         React and Tailwind and what is really going onnn!
       </h1>
+      <button className="border border-black p-3 m-3 hover:bg-gray-300">
+        Click here
+      </button>
     </div>
   );
 }
