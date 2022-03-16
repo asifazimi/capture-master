e12ee623a5dd101305f377a1e129c81ab1b00ea0
Asif Azimi <asifazimi620@gmail.com>
2022-03-16 06:07:57 +0430

    App changed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
       <h1 className="text-3xl font-bold underline text-pink-500">
         React and Tailwind and what is really going onnn!
       </h1>
-      <button className="border border-black p-3 m-3 hover:bg-gray-300">
-        Click here
-      </button>
       <button className="font-bold border rounded px-3 py-2 border-red-500 transition-all ease-out hover:bg-pink-500 hover:text-white ">
         Submit me
       </button>
