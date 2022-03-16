f31f5ebead3286ae479764ad07b756be2c8bc4e7
Asif Azimi <asifazimi620@gmail.com>
2022-03-16 09:44:17 +0430

    commit message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline text-pink-500">
         React and Tailwind and what is really going onnn!
+        <span className="text-2xl font-black underline text-red-800">
+          Here is the list
+        </span>
       </h1>
     </div>
   );
