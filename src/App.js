11ccbf438b2f518ec48e17c1ed80381f206fb08a
Asif Azimi <asifazimi620@gmail.com>
2022-03-18 16:00:14 +0430

    file changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-pink-500">
-        React and Tailwind and what is really going onnn!
-      </h1>
+      <ul role="list" class="p-6 divide-y divide-slate-200">
+        <li class="flex py-4 first:pt-0 last:pb-0">
+          <img
+            class="h-10 w-10 rounded-full"
+            src={"./img/asif.jpeg"}
+            alt="Image"
+          />
+          <div class="ml-3 overflow-hidden">
+            <p class="text-sm font-medium text-slate-900">Mohammad Asif</p>
+            <p class="text-sm text-slate-500 truncate">
+              iasifazimi@outlook.com
+            </p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
