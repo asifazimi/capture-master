ab98e72463239d5775661a52452db43dc8fb84c0
Asif Azimi <asifazimi620@gmail.com>
2022-03-18 15:41:32 +0430

    File changed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline text-pink-500">
         React and Tailwind and what is really going onnn!
-        <span className="text-2xl font-black underline text-red-800">
-          Here is the list for you guys hope you enjoy it. and yes.
-        </span>
       </h1>
     </div>
   );
