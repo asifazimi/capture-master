2eb3cdbdd5e065edd17f154683ee5b92c50b64f7
Asif Azimi <asifazimi620@gmail.com>
2022-03-18 16:04:30 +0430

    file changed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <ul role="list" class="p-6 divide-y divide-slate-200">
-        <li class="flex py-4 first:pt-0 last:pb-0">
-          <img
-            class="h-10 w-10 rounded-full"
-            src={"./img/asif.jpeg"}
-            alt="Image"
-          />
-          <div class="ml-3 overflow-hidden">
-            <p class="text-sm font-medium text-slate-900">Mohammad Asif</p>
-            <p class="text-sm text-slate-500 truncate">
-              iasifazimi@outlook.com
-            </p>
-          </div>
-        </li>
-      </ul>
+      <p>Hi</p>
     </div>
   );
 }
