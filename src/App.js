85fa8d519e45edce8dda6d318a9bc3f1bf703ad9
Asif Azimi <asifazimi620@gmail.com>
2022-03-23 10:10:26 +0430

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // Import pages
 import Aboutus from "./pages/AboutUs";
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Aboutus />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,69 @@
 import home1 from "../img/home1.png";
+// Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills to help you achive it.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
-        <img src={home1} alt="home1" />
-      </div>
-    </div>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with  a camera" />
+      </Image>
+    </About>
   );
 };
+
+// Styled component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
